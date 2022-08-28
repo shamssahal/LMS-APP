bf97fae180ea5583aa906b39ba12aca15d84a85a
Sahal Shams <shamssahal@gmail.com>
2022-08-29 01:53:01 +0530

    Config changes

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,7 @@
-export let ROOT_URL = 'http://localhost:3000';
-export const AWS_S3_USER = 'https://d1caq8coktrxsi.cloudfront.net'
+export let ROOT_URL;
+if (process.env.REACT_APP_ENV === "production") {
+    ROOT_URL = "http://44.201.234.109";
+} else {
+    ROOT_URL = "http://localhost:3000";
+}
+export const AWS_S3_USER = 'https://shams-bucket.s3.amazonaws.com'
